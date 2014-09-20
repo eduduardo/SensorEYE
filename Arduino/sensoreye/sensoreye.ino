d5ad1954c41743bed7f676958e9422d127709401
Eduardo Ramos <eduardo.ramos@hotmail.com.br>
2014-09-20 14:13:08 -0300

    Miscelânea de modificações

--- Arduino/sensoreye/sensoreye.ino
+++ Arduino/sensoreye/sensoreye.ino
@@ -25,6 +25,7 @@ const int pinoLed = 4; // led usada para os testes iniciais
 const int pinoVibracall = 5; // vibracall
 
 // const float velocidadeSom = 346.3 // velocidade do som em (m/s) a +25 °C
+byte comandoSerial;
 
 Ultrasonic ultrasonic(pinoDisparador, pinoEco);
 
@@ -32,13 +33,12 @@ void setup() {
   Serial.begin(9600); // inicia o serial
   
   // mensagem inicial
-  Serial.println(); // nova linha
   Serial.println("Projeto SensorEYE");
   Serial.println("Escrito por Eduardo Ramos, Filipe Gianotto e Felipe Pereira @ 1 EMIA");
   Serial.println("Versao: 0.0 ALPHA");
   Serial.println("Compilado em " __DATE__ " as " __TIME__);
   Serial.println("Pressione h para /help");
-  Serial.println();
+  Serial.println(); // nova linha
   
   pinMode(pinoEco, INPUT);
   pinMode(pinoDisparador, OUTPUT);
@@ -48,24 +48,35 @@ void setup() {
 }
  
 void loop() {
-   float distancia = calcular();
-   
-   if(distancia >= 2 && distancia <= 5) {
-      vibracall(100); 
+   // verifica se há comandos no Serial
+   if(Serial.available() > 0){
+      comandoSerial = Serial.read(); 
    }
+   
+   // comandos do Serial
+   switch(comandoSerial){
+       
+   }
+   comandoSerial = 0;
+   
+   float distancia = calcular();
    
    delay(100);
 }
 
 // funções internas
-void help() { }
+void help() { 
+  Serial.println("");
+}
 
 void inicializar() {
   // dois beeps/vibrações iniciais para teste
   for (int i = 0; i < 2; i++){
      vibracall(200);
-     delay(2); 
-  }
+     delay(1000);
+     desligaVibracall();
+     delay(1000); 
+  }   
 }
 
 // funcao do ultrasonico
@@ -84,9 +95,31 @@ float calcular() {
     return distanciaCM;
 }
 
-// linguagem para os deficientes visuais
 void vibracall(int intensidade) {
   analogWrite(pinoVibracall, intensidade); // 1-255
 }
 
+void desligaVibracall() {
+   vibracall(0);
+}
+
 void fonebeep(int intensidade) { }
+
+void linguagem(int distancia) {
+    /*
+     distancia em CM
+     1 M = 100 CM
+     */
+    float centimetros = distancia;
+    float metros = centimetros * 100;
+    
+    // objetos pertos (em cm)
+    if(distancia < 100){
+    
+    
+    
+    // objetos distantes (em metros)  
+    } else {      
+      
+    }
+}
